5972fb3c37a1f1e5169813c63e6ae004276c87a1
Bianca Severin <bianca.severin@gmail.com>
2022-05-29 15:55:23 +0200

    added API Key for the Map Window

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,11 @@ namespace ViennaMaps
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // API Key Bianca
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "AAPK45946aac033744e7829cc44189d86fedZF6POt8sn3u9iO_fWel9o9FEUXH2upCLXgnOOGASVnVYHEy4DyuJjH3m4aljQmv_";
+        }
     }
 }
--- Views/MapWindow.xaml.cs
+++ Views/MapWindow.xaml.cs
@@ -12,22 +12,96 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Data;
 
 namespace ViennaMaps.Views
 {
     /// <summary>
     /// Interaktionslogik für MapWindow.xaml
     /// </summary>
-    public partial class MapWindow : Window
+    /// 
+  
+
+    public partial class MapWindow: Window
     {
         public MapWindow()
         {
             InitializeComponent();
-            Map myMap = new Map(Basemap.CreateDarkGrayCanvasVector());
+            Initialize();
+            /*Map myMap = new Map(Basemap.CreateDarkGrayCanvasVector());
 
             ArcGISMapImageLayer layer = new ArcGISMapImageLayer(new Uri("https://services.arcgis.com/LG9Yn2oFqZi5PnO5/arcgis/rest/services/xgqZm/FeatureServer"));
             myMap.OperationalLayers.Add(layer);
-            MyMapView.Map = myMap;
+            MyMapView.Map = myMap;*/
         }
+
+        private void Initialize()
+        {
+           
+            // Create a new Scene with a topographic basemap.
+            Scene scene = new Scene(BasemapStyle.ArcGISTopographic);
+
+            // Add a base surface with elevation data.
+            Surface elevationSurface = new Surface();
+            Uri elevationService = new Uri("https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer");
+            elevationSurface.ElevationSources.Add(new ArcGISTiledElevationSource(elevationService));
+            scene.BaseSurface = elevationSurface;
+
+            // Add a scene layer.
+            Uri buildingsService = new Uri("https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Buildings_Brest/SceneServer/layers/0");
+            ArcGISSceneLayer buildingsLayer = new ArcGISSceneLayer(buildingsService);
+            scene.OperationalLayers.Add(buildingsLayer);
+
+            // Assign the Scene to the SceneView.
+            MySceneView.Scene = scene;
+
+            try
+            {
+                // Create a camera with an interesting view.
+                Camera viewCamera = new Camera(48.378, -4.494, 200, 345, 65, 0);
+
+                // Set the viewpoint with the camera.
+                MySceneView.SetViewpointCamera(viewCamera);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error");
+            }
+
+        }
+        private async void SceneViewTapped(object sender, Esri.ArcGISRuntime.UI.Controls.GeoViewInputEventArgs e)
+        {
+            // Get the scene layer from the scene (first and only operational layer).
+            ArcGISSceneLayer sceneLayer = (ArcGISSceneLayer)MySceneView.Scene.OperationalLayers.First();
+
+            // Clear any existing selection.
+            sceneLayer.ClearSelection();
+
+            try
+            {
+                // Identify the layer at the tap point.
+                // Use a 10-pixel tolerance around the point and return a maximum of one feature.
+                IdentifyLayerResult result = await MySceneView.IdentifyLayerAsync(sceneLayer, e.Position, 10, false, 1);
+
+                // Get the GeoElements that were identified (will be 0 or 1 element).
+                IReadOnlyList<GeoElement> geoElements = result.GeoElements;
+
+                // If a GeoElement was identified, select it in the scene.
+                if (geoElements.Any())
+                {
+                    GeoElement geoElement = geoElements.FirstOrDefault();
+                    if (geoElement != null)
+                    {
+                        // Select the feature to highlight it in the scene view.
+                        sceneLayer.SelectFeature((Feature)geoElement);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+        }
+
     }
 }

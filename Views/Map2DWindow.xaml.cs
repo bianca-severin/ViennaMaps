dd85849f7d16049ed1fa3a9aed498b122d535277
Bianca Severin <severin@boanet.at>
2022-06-01 17:54:18 +0200

    added two new layers to the 2d map

--- Views/Map2DWindow.xaml.cs
+++ Views/Map2DWindow.xaml.cs
@@ -40,12 +40,16 @@ namespace ViennaMaps.Views
             FeatureLayer devOne = new FeatureLayer(new Uri("https://services1.arcgis.com/YfxQKFk1MjjurGb5/ArcGIS/rest/services/B%c3%bcchereien/FeatureServer/0"));
             //Gemeindebauten heute
             FeatureLayer devTwo = new FeatureLayer(new Uri("https://services.arcgis.com/E3H3dLmHwo799BQr/ArcGIS/rest/services/Gemeindebauten_Wien_Vollst%c3%a4ndig_WFL1/FeatureServer/0"));
-            //
+            //Fußgängerzonen
             FeatureLayer devThree = new FeatureLayer(new Uri("https://services.arcgis.com/Sf0q24s0oDKgX14j/ArcGIS/rest/services/Fu%c3%9fg%c3%a4ngerzonen_Wien_WFL1/FeatureServer/0"));
+            //Bezirksgrenzen
             FeatureLayer nonDevOne = new FeatureLayer(new Uri("https://services.arcgis.com/E3H3dLmHwo799BQr/ArcGIS/rest/services/Gemeindebauten_Wien_Vollst%c3%a4ndig_WFL1/FeatureServer/5"));
+            //Lärm innerbezirke
             FeatureLayer nonDevTwo = new FeatureLayer(new Uri("https://services.arcgis.com/E3H3dLmHwo799BQr/ArcGIS/rest/services/Wien_Nacht_Stra%c3%9fe/FeatureServer/12"));
-
-           
+            //Verkehr (loads super slow)
+           /* FeatureLayer Verkehr = new FeatureLayer(new Uri("https://services1.arcgis.com/YfxQKFk1MjjurGb5/arcgis/rest/services/GIP_Verkehrsnetz/FeatureServer/0"));*/
+            //Hiking Trails
+            FeatureLayer HikingTrails = new FeatureLayer(new Uri("https://services2.arcgis.com/Eij2WK0CHxEthqHo/ArcGIS/rest/services/Vienna_City_Trails%e6%b5%8b%e8%af%95/FeatureServer/0"));
 
             // Create the group layer and add sublayers.
             GroupLayer gLayer = new GroupLayer();
@@ -61,14 +65,16 @@ namespace ViennaMaps.Views
              MyMap2DView.Scene.OperationalLayers.Add(gLayer);
              MyMap2DView.Scene.OperationalLayers.Add(nonDevOne);
              MyMap2DView.Scene.OperationalLayers.Add(nonDevTwo);
+             MyMap2DView.Scene.OperationalLayers.Add(HikingTrails);
 
-            
+          
 
-            // Wait for all of the layers in the group layer to load.
-            await Task.WhenAll(gLayer.Layers.ToList().Select(m => m.LoadAsync()).ToList());
+
+           // Wait for all of the layers in the group layer to load.
+           await Task.WhenAll(gLayer.Layers.ToList().Select(m => m.LoadAsync()).ToList());
 
             // Zoom to the extent of the group layer.
-            MyMap2DView.SetViewpoint(new Viewpoint(devOne.FullExtent));
+            MyMap2DView.SetViewpoint(new Viewpoint(nonDevOne.FullExtent));
 
         }
        
